9b529ec05cbcfd2ddca9bdd990648469435fff8b
Jordan Amdahl <jamdahl@Carthage.local>
2020-08-22 12:33:21 -0400

    Prep for 1.2.0

--- tests/testthat/test_surv_funcs.R
+++ tests/testthat/test_surv_funcs.R
@@ -17,6 +17,17 @@ test_that("Mean survival works", {
   nmix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="logistic", dist="llogis", mixture = F)
   nmix_some_cured_res <- summary(nmix_some_cured, type = "mean", tidy=T)
   expect_equal(as.numeric(nmix_some_cured_res), c(Inf, Inf, Inf))
+
+
+  # Test case where mean is zero
+  expect_equal(
+    mean_nmixsurv(pgenf, 0, mu = 1.2, sigma = 0.8, Q = 0.2, P = 0.3),
+    mean_genf(1.2, 0.8, 0.2, 0.3)
+  )
+  expect_equal(
+    mean_mixsurv(pgenf, 0, mu = 1.2, sigma = 0.8, Q = 0.2, P = 0.3),
+    mean_genf(1.2, 0.8, 0.2, 0.3)
+  )
 })
 
 test_that("RMST Works", {
@@ -34,47 +45,61 @@ test_that("RMST Works", {
     mix_some_cured_res$est,
     (1 - mix_some_cured$res[1,1]) * mix_some_cured_res_u + mix_some_cured$res[1,1] * t_rmst
   )
+
+  nmix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="logistic", dist="gompertz", mix = F)
+  nmix_some_cured_res <- summary(nmix_some_cured, t=t_rmst, type = "rmst", tidy=T)
+  expect_equal(
+    nmix_some_cured_res$est,
+    integrate(function(x) pnmixsurv(
+      pgompertz,
+      x,
+      nmix_some_cured$res[1,1],
+      shape = nmix_some_cured$res[2,1],
+      rate = nmix_some_cured$res[3,1],
+      lower.tail = F), 0, t_rmst
+    )$value
+  )
 })
 
 test_that("Survival projections", {
 
   # MIXTURE MODELS
-  # Survival at t = Inf should equal cure fraction
+  # Survival should be equal to cure fraction for large values of t
   mix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="loglog", dist="lnorm")
-  mix_some_cured_res <- summary(mix_some_cured, t=Inf, type = "survival", tidy=T)
+  mix_some_cured_res <- summary(mix_some_cured, t=1e99, type = "survival", tidy=T)
   expect_equal(as.numeric(mix_some_cured_res)[2], as.numeric(mix_some_cured$res[1]))
 
   # NON-MIXTURE MODELS
-  # Mean should be infinite for models with positive cure fraction
+  # Survival should be equal to cure fraction for large values of t
   nmix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="loglog", dist="lnorm", mixture=F)
-  nmix_some_cured_res <- summary(nmix_some_cured, t=Inf, type = "survival", tidy=T)
+  nmix_some_cured_res <- summary(nmix_some_cured, t=1e99, type = "survival", tidy=T)
   expect_equal(as.numeric(nmix_some_cured_res)[2], as.numeric(nmix_some_cured$res[1]))
 })
 
 test_that("Cumulative hazard projections", {
 
   # MIXTURE MODELS
-  # Cumulaitve hazard at t = Inf should equal -log(cure fraction0
+  # Cumulative hazard should equal -log(cure fraction) for large values of t
   mix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="identity", dist="weibull")
-  mix_some_cured_res <- summary(mix_some_cured, t=Inf, type = "cumhaz", tidy=T)
+  mix_some_cured_res <- summary(mix_some_cured, t=1e99, type = "cumhaz", tidy=T)
   expect_equal(as.numeric(mix_some_cured_res)[2], -log(as.numeric(mix_some_cured$res[1])))
 
   # NON-MIXTURE MODELS
-  # Cumulaitve hazard at t = Inf should equal -log(cure fraction0
+  # Cumulative hazard should equal -log(cure fraction) for large values of t
   nmix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="identity", dist="weibull", mixture=F)
-  nmix_some_cured_res <- summary(nmix_some_cured, t=Inf, type = "cumhaz", tidy=T)
+  nmix_some_cured_res <- summary(nmix_some_cured, t=1e99, type = "cumhaz", tidy=T)
   expect_equal(as.numeric(nmix_some_cured_res)[2], -log(as.numeric(nmix_some_cured$res[1])))
 
   # MIXTURE MODELS
   # Cumulative hazard should flatten
   mix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="logistic", dist="exp")
-  mix_some_cured_res <- summary(mix_some_cured, t=c(9999999, Inf), type = "cumhaz", tidy=T)
+  mix_some_cured_res <- summary(mix_some_cured, t=c(9999999, 1e99), type = "cumhaz", tidy=T)
   expect_equal(mix_some_cured_res$est[1], mix_some_cured_res$est[2])
 
   # NON-MIXTURE MODELS
   # Cumulative hazard should flatten
   nmix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="logistic", dist="exp", mixture=F)
-  nmix_some_cured_res <- summary(nmix_some_cured, t=c(9999999, Inf), type = "cumhaz", tidy=T)
+  nmix_some_cured_res <- summary(nmix_some_cured, t=c(9999999, 1e99), type = "cumhaz", tidy=T)
   expect_equal(nmix_some_cured_res$est[1], nmix_some_cured_res$est[2])
 })
 
@@ -104,7 +129,20 @@ test_that("Random sampling", {
 
 })
 
+test_that("P function works with infinite input", {
 
+  # MIXTURE MODELS
+  # Hazard at t = Inf should be zero
+  mix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="logistic", dist="exp")
+  mix_some_cured_res <- summary(mix_some_cured, t=Inf, type = "survival", tidy=T)
+  expect_equal(as.numeric(mix_some_cured_res)[2], 0)
+
+  # NON-MIXTURE MODELS
+  # Hazard at t = Inf should be zero
+  nmix_some_cured <- flexsurvcure(Surv(rectime, censrec)~1,data=bc,link="logistic", dist="exp", mixture=F)
+  nmix_some_cured_res <- summary(nmix_some_cured, t=Inf, type = "survival", tidy=T)
+  expect_equal(as.numeric(nmix_some_cured_res)[2], 0)
+})
 
 test_that("Quantile functions", {
 
